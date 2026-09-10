51403772cddff22bd28f60f0577c7c9213d42dc1
garrett <garrett.lane1985@gmail.com>
2026-09-10 08:06:19 +0200

    added content to index and some basic styles

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,11 @@ const copyrightYear = document.querySelector('#copyright-year');
 if (copyrightYear) {
   copyrightYear.textContent = new Date().getFullYear();
 }
+
+document.getElementById('cipherBtn').addEventListener('click', () => {
+  const text = document.getElementById('cipherInput').value;
+  const shift = parseInt(document.getElementById('shiftInput').value);
+
+  const encrypted = caesarCipher(text, shift);
+  document.getElementById('cipherOutput').innerText = encrypted;
+});
