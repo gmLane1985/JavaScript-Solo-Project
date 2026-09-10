f62559ba4788bafabb538d46ebceb5a562c1ce04
garrett <garrett.lane1985@gmail.com>
2026-09-10 08:12:13 +0200

    added javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,10 +6,38 @@ if (copyrightYear) {
   copyrightYear.textContent = new Date().getFullYear();
 }
 
-document.getElementById('cipherBtn').addEventListener('click', () => {
-  const text = document.getElementById('cipherInput').value;
-  const shift = parseInt(document.getElementById('shiftInput').value);
+//caesar
+
+function caesarCipher(text, shift) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const lowerText = text.toLowerCase();
+  let result = '';
+
+  for (let i = 0; i < lowerText.length; i++) {
+    const char = lowerText[i];
+
+    if (!alphabet.includes(char)) {
+      result += char;
+      continue;
+    }
+
+    const oldIndex = alphabet.indexOf(char);
+    let newIndex = (oldIndex + shift) % 26;
+
+    if (newIndex < 0) {
+      newIndex += 26;
+    }
+
+    result += alphabet[newIndex];
+  }
+
+  return result;
+}
+
+document.getElementById('caesar_cipherBtn').addEventListener('click', () => {
+  const text = document.getElementById('caesar_cipherInput').value;
+  const shift = parseInt(document.getElementById('caesar_shiftInput').value);
 
   const encrypted = caesarCipher(text, shift);
-  document.getElementById('cipherOutput').innerText = encrypted;
+  document.getElementById('caesar_cipherOutput').innerText = encrypted;
 });
